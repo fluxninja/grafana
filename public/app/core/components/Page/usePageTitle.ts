7d1e92ccd59e3c01354e128504abcdb9e3a67a80
Gurinder Singh <gurinder.rawala@fluxninja.com>
2022-08-15 18:38:38 -0400

    fixed pageTitle

--- public/app/core/components/Page/usePageTitle.ts
+++ public/app/core/components/Page/usePageTitle.ts
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import { NavModel, NavModelItem } from '@grafana/data';
 
 import { Branding } from '../Branding/Branding';
 
 export function usePageTitle(navModel?: NavModel, pageNav?: NavModelItem) {
+  const { FNDashboard, pageTitle } = useSelector((state) => state.fnGlobleState);
   useEffect(() => {
     const parts: string[] = [];
     if (pageNav) {
@@ -23,11 +25,9 @@ export function usePageTitle(navModel?: NavModel, pageNav?: NavModelItem) {
       }
       addTitleSegment(parts, navModel.main);
     }
-
     parts.push(Branding.AppTitle);
-
-    document.title = parts.join(' - ');
-  }, [navModel, pageNav]);
+    document.title = !FNDashboard ? parts.join(' - ') : pageTitle ? pageTitle : 'Flux Ninja';
+  }, [navModel, pageNav, FNDashboard, pageTitle]);
 }
 
 function addTitleSegment(parts: string[], node: NavModelItem) {
