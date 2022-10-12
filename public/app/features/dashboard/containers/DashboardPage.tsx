441dee13c35f2a96bc4e3a37463ba0de9ee8e732
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek-zdanowicz@fluxninja.com>
2022-10-12 13:09:13 +0200

    Revert "Merge pull request #13 from fluxninja/GH-6578-use-fn-loader"
    This reverts commit 323f6676535245f9ab94b4fb17830d1e3a9cbea1, reversing
    changes made to dc23c1287dac743b8add889dd03980e894345d11.

--- public/app/features/dashboard/containers/DashboardPage.tsx
+++ public/app/features/dashboard/containers/DashboardPage.tsx
@@ -97,11 +97,7 @@ type OwnProps = {
   isFNDashboard?: boolean;
   controlsContainer?: HTMLElement | null;
   hiddenVariables?: string[];
-  fnLoader?: ReactNode;
 };
-
-export type DashboardPageProps = OwnProps &
-  GrafanaRouteComponentProps<DashboardPageRouteParams, DashboardPageRouteSearchParams>;
 
 export type Props = OwnProps &
   Themeable2 &
